219f9ae200f3eb0460cff9fabe0e70b42a1f15ad
Prabhu <Sudharsan.Prabhu@lfg.com>
2018-10-24 20:38:47 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var newGame = document.querySelector(".newGame");
+var diceScore = document.querySelector("#diceScore");
+var playTurnP1=document.querySelector("#playTurnP1");
+var playTurnP2=document.querySelector("#playTurnP2");
+var totalScoreP1=document.querySelector("#totalScoreP1");
+var totalScoreP2=document.querySelector("#totalScoreP2");
+var currentScoreP1=document.querySelector("#currentScoreP1");
+var currentScoreP2=document.querySelector("#currentScoreP2");
+var roleDice= document.querySelector("#roleDice");
+var holdAndPass=document.querySelector("#holdAndPass");
+
